be348e7afc1f2c97f5206be34719028ef245eb89
Rosie <xiluorosise67@gmail.com>
2025-01-03 02:42:51 -0500

    pp_postdoc multi label updating rule

--- PP_backproj.py
+++ PP_backproj.py
@@ -382,23 +382,51 @@ def find_txt_files(base_dir):
                     label_files.append(os.path.join(root, file))
     return txt_files, label_files
 
+from collections import Counter
+
 class pj:
     def __init__(self, cloud):
         pj.cloud = -np.ones([cloud.shape[0], 7])
         pj.cloud[:, 0:6] = cloud
+        pj.weight = {'-1': 0, '0': 1, '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '7': 1, '8': 1, '9': 1, '10': 1, '11': 1, '12': 1, '13': 1}
         pj.priority = {'-1': 0, '0': 1, '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '7': 1, '8': 1, '9': 1, '10': 1, '11': 1, '12': 1, '13': 1}
+        pj.temp_label = [[-1 for _ in range(1)] for _ in range(pj.cloud.shape[0])]
 
-    def predict_labels(self, labels):
+    def predict_labels_weighted(self, labels):
         for sub in labels:
             for new_label, index in sub:  # Process each valid point and its label
                 current_label = self.cloud[int(index), -1]  # Assume labels are stored in the last column
-                current_priority = self.priority.get(str(int(current_label)), float('-inf'))
-                new_priority = self.priority.get(str(int(new_label)), float('-inf'))
+                current_priority = self.weight.get(str(int(current_label)), float('-inf'))
+                new_priority = self.weight.get(str(int(new_label)), float('-inf'))
 
                 # Update the label if the new one has higher priority
                 if new_priority > current_priority:
                     self.cloud[int(index), -1] = new_label
         return self.cloud
+    
+    def update_most_voted_label(self):
+        for i, l in enumerate(self.temp_label):
+            new_label = Counter(l).most_common(1)[0][0] #Get the most voted label
+            # Try to avoid the filled -1
+            new_priority = self.priority.get(str(int(new_label)), float('-inf'))
+            current_label = self.cloud[int(i), -1]  # Assume labels are stored in the last column
+            current_priority = self.priority.get(str(int(current_label)), float('-inf'))
+
+            # Update the label if the new one has higher priority
+            if new_priority > current_priority:
+                self.cloud[int(i), -1] = new_label
+
+        return
+
+    def predic_labels_appearance(self, labels):
+        for sub in labels:
+            for new_label, index in sub:  # Process each valid point and its label
+                self.temp_label[index].append(new_label)
+
+        self.update_most_voted_label()
+        
+        return self.cloud
+
 
 
 def get_3d_eval_res(predicted_labels, ground_truth_labels):
@@ -497,48 +525,3 @@ def get_3d_eval_res(predicted_labels, ground_truth_labels):
     return overall_accuracy, overall_precision, overall_recall, overall_iou
 
 from CNN import *
-
-# Pre-define the view params (location, view angle... ,etc.)
-width, height = 512, 512
-# fov = np.pi / 1.4  # 60 degrees
-near, far = 0.1, 1000
-aspect_ratio = width / height
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = UNet(num_classes=14).to(device)
-if torch.cuda.device_count() >= 1:
-    print(f"Let's use {torch.cuda.device_count()} GPUs!")
-    model = nn.DataParallel(model)
-    model = model.to(device)
-    model = model.cuda()
-model.load_state_dict(torch.load('/home/xi/repo/VGG/log/model_UNET_PP3_LR1.2-04.pth'))
-model.eval()
-
-
-IoU_res = []
-# ------------------------------------------------------------------------------------
-base_dir = '/home/xi/repo/Stanford3dDataset_v1.2_Aligned_Version_Test/'
-scan_files, scan_labels = find_txt_files(base_dir)
-
-# Print all .txt file paths
-for txt_file in scan_files:
-    print(txt_file)
-
-for i in np.arange(len(scan_files)):
-    scan_path = scan_files[i]
-    label_path = scan_labels[i]
-
-    scan = np.loadtxt(scan_path, dtype=np.float32)
-    # scan = scan.reshape((-1, 6))
-    label = np.loadtxt(label_path, dtype=np.float32)
-    l = gen_the_pp_image(scan=scan, label=label, scan_path=scan_path)
-
-    bj = pj(scan)
-    extended_points_with_labels = bj.predict_labels(l)
-    # extended_points_with_labels = bj.predict_labels(image_mask, proj_W=512, proj_H=512, proj_fov_up=110, proj_fov_down = -110)
-    _, _, _, IoU = get_3d_eval_res(extended_points_with_labels[:, -1], label)
-
-    IoU_res.append(IoU)
-
-
-print(np.mean(IoU_res))
